2bd0fdeb95af90cadde24792d889b97064d8c86b
MitchelRos <mrosado19@jaumebalmes.net>
2022-09-29 19:41:36 +0200

    Los getter, setters, toString y funcions

--- Vehicle.php
+++ Vehicle.php
@@ -26,6 +26,67 @@ class Vehicle
     {
     }
 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
+
+    public function getKm()
+    {
+        return $this->km;
+    }
+
+    public function setKm($km)
+    {
+        $this->km = $km;
+    }
+
+    public function getCavalls()
+    {
+        return $this->cavalls;
+    }
+
+    public function setCavalls($cavalls)
+    {
+        $this->cavalls = $cavalls;
+    }
+
+    public function getAntiguitat()
+    {
+        return $this->antiguitat;
+    }
+
+    public function setAntiguitat($antiguitat)
+    {
+        $this->antiguitat = $antiguitat;
+    }
+    public function __toString()
+    {
+        return "NOM: {$this->nom} MODEL: {$this->model} KM: {$this->km} CABALLS: {$this->cavalls} ANTIGUITAT: {$this->antiguitat}";
+    }
+    public function kmRecorregut()
+    {
+        return rand(1,100);
+    }
+    public function vehicleCreat()
+    {
+        return "MODEL: {$this->model} CABALLS: {$this->cavalls}";
+    }
 }
 
 ?>
