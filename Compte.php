ef0afc4259c165e4b2f80f7a1db9bea47c01edb1
ksama22 <ksama22@jaumebalmes.net>
2022-09-29 20:05:50 +0200

    Compte

---
+++ Compte.php
@@ -0,0 +1,52 @@
+<?php
+
+class Compte
+{
+    private $nomUsuari;
+    private $diners;
+
+    public function __construct($nomUsuari, $diners)
+    {
+        $this->nomUsuari = $nomUsuari;
+        $this->diners = $diners;
+    }
+
+    public function getNomUsuari()
+    {
+        return $this->nomUsuari;
+    }
+
+
+    public function setNomUsuari($nomUsuari)
+    {
+        $this->nomUsuari = $nomUsuari;
+    }
+
+
+    public function getDiners()
+    {
+        return $this->diners;
+    }
+
+
+    public function setDiners($diners)
+    {
+        $this->diners = $diners;
+    }
+
+}
+
+
+$bar = new Compte("Kevin", 1700);
+$treuDiners = 100;
+$dinersActuals = $bar->getDiners();
+$ingresarDiners = 500;
+
+echo " {$bar->getNomUsuari()} te {$bar->getDiners()} diners<br>";
+$bar->setDiners($dinersActuals - $treuDiners);
+echo " {$bar->getNomUsuari()} te {$bar->getDiners()} diners<br>";
+$bar->setDiners($dinersActuals + $ingresarDiners);
+echo " {$bar->getNomUsuari()} te {$bar->getDiners()} diners<br>";
+
+
+?>
